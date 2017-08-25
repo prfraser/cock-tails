e64c350254d241356bc7fee9a84931556fb867de
philip fraser <pr.fraser3@gmail.com>
2017-08-25 17:52:37 +1000

    added methods

--- cocktails.rb
+++ cocktails.rb
@@ -1,30 +1,29 @@
-recipe_hash = {
-
-	old_fashioned = {
-	spirits: [:whiskey],
-	other: [:bitters, :sugar_cube, :water]
-	},
-
-	margarita = {
-	spirits: [:tequila, :cointreau],
-	other: [:lime_juice]
-	},
-
-	negroni = {
-	spirits: [:gin, :campari, :sweet_vermouth]
-	},
-
-	french_75 = {
-	spirits: [:champagne, :gin],
-	other: [:lemon_juice, :simple_syrup]
-		},
-
-	manhattan = {
-	spirits: [:whiskey, :vermouth],
-	other: [:bitters, :cherry]
-	},
-
-	gin_and_tonic = {
-	spirits: [:gin],
-	other: [:tonic, :lime, :cucumber]
+recipe_find_hash = {
+	old_fashioned: [:whiskey],
+	margarita: [:tequila, :cointreau],
+	negroni: [:gin, :campari, :sweet_vermouth],
+	french_75: [:champagne, :gin],
+	manhattan: [:whiskey, :vermouth],
+	gin_and_tonic: [:gin],
 }
+
+$recipe_hash = {
+	old_fashioned: ["Whiskey", "Make the drink"],
+	margarita: ["Tequila", "cointreau", "Make the drink"],
+	negroni: ["gin", "campari", "sweet_vermouth", "blah blah blah"],
+	french_75: ["champagne", "gin"],
+	manhattan: ["whiskey", "vermouth", "asdf asdfasdfasdf asdfasdf"],
+	gin_and_tonic: ["gin", "dafsdf"],
+}
+
+def find_recipe(recipe)
+	puts $recipe_hash[recipe]
+end
+
+puts "Give a spirit: "
+$user_choice = gets.chomp.to_sym
+
+recipe_find_hash.each do |k,v|
+	puts find_recipe(k) if v.include? $user_choice
+end
+
