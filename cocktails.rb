36f4df9ce3f5e4d757175e823f9e7f7c97552192
calebharnell <calebharnell@hotmail.com>
2017-08-25 17:33:44 +1000

    Added case statement

--- cocktails.rb
+++ cocktails.rb
@@ -1,18 +1,62 @@
-old_fashioned = {
-	spirits: [:whiskey],
-	other: [:bitters, :sugar_cube, :water]
+recipe_hash = {
+
+	old_fashioned: [:whiskey],
+	margarita: [:tequila, :cointreau],
+	negroni: [:gin, :campari, :sweet_vermouth],
+	french_75: [:champagne, :gin],
+	manhattan: [:whiskey, :sweet_vermouth],
+	gin_and_tonic: [:gin],
+	screwdriver: [:vodka],
+	martini: [:gin, :dry_vermouth],
+	cosmopolitan: [:cointreau, :vodka_citron],
+	stinger: [:cognac, :creme_de_menthe],
+	americano: [:campari, :red_vermouth],
+	white_russian: [:coffee_liquer, :vodka],
+	daiquiri: [:rum],
+	bloody_mary: [:vodka],
+
 }
 
-margarita = {
-	spirits: [:tequila, :cointreau],
-	other: [:lime_juice]
-}
+puts "Enter 1 for whiskey			Enter 9 for dry vermouth"
+puts "Enter 2 for rum				Enter 10 for sweet vermouth"
+puts "Enter 3 for vodka			Enter 11 for red vermouth"
+puts "Enter 4 for tequila			Enter 12 for vodka citron"
+puts "Enter 5 for gin				Enter 13 for cognac"
+puts "Enter 6 for cointreau			Enter 14 for creme de menthe"
+puts "Enter 7 for campari			Enter 15 for coffee liqueur"
+puts "Enter 8 for champagne"
 
-negroni = {
-	spirits: [:gin, :campari, :sweet_vermouth]
-}
+$user_choice = gets.chomp.to_i
 
-manhattan = {
-	spirits: [:whiskey, :vermouth],
-	other: [:bitters, :cherry]
-}
+case $user_choice
+	when 1
+		puts :whiskey
+	when 2
+		puts :rum
+	when 3
+		puts :vodka
+	when 4
+		puts :tequila
+	when 5
+		puts :gin
+	when 6
+		puts :cointreau
+	when 7
+		puts :campari
+	when 8
+		puts :champagne
+	when 9
+		puts :dry_vermouth
+	when 10
+		puts :sweet_vermouth
+	when 11
+		puts :red_vermouth
+	when 12
+		puts :vodka_citron
+	when 13
+		puts :cognac
+	when 14
+		puts :creme_de_menthe
+	when 15
+		puts :coffee_liquer
+	end
